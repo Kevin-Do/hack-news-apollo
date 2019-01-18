bab1f3d490f0197cd2764e1d41ba0d3cd824c219
Kevin Do <dokev@justin.tv>
2019-01-18 11:37:16 -0800

    Local Data Link List

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import LinkList from './components/LinkList';
+
 import logo from './logo.svg';
 import './styles/App.css';
 
@@ -9,7 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <LinkList />
           </p>
           <a
             className="App-link"
