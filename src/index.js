f699bcd55a0e6f6e843dbb71787b508451f5e028
Kevin Do <dokev@justin.tv>
2019-01-22 10:42:24 -0800

    Subscription to votes and new posts

--- src/index.js
+++ src/index.js
@@ -13,9 +13,24 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context'
 
+
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN),
+        }
+    }
+})
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem(AUTH_TOKEN)
@@ -27,8 +42,17 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: link,
     cache: new InMemoryCache()
 });
 
