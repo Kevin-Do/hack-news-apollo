c375a8bc5ff3dd78478ae01f7277814f8b90a238
Kevin Do <dokev@justin.tv>
2019-01-22 10:14:08 -0800

    Adds auth token middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { AUTH_TOKEN } from './constants'
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,13 +11,24 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
